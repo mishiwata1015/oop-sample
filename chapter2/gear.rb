df83b9b05488512a0bca12efbed8d41c3262a275
Masanori Ishiwata <mishiwata1015@gmail.com>
2020-04-09 00:11:24 +0900

    Add gear_inches

--- chapter2/gear.rb
+++ chapter2/gear.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 class Gear
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
     chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * (rim + (tire * 2))
   end
 end
